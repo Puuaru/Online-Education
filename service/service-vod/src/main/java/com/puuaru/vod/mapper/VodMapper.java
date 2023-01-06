f1c0ab497056d5a7d8eeaeed44e90f72b62e536c
Puuaru <honghuizhang4w@gmail.com>
2023-01-06 17:31:36 +0800

    feature(edu): video update, delete, playAuth

---
+++ service/service-vod/src/main/java/com/puuaru/vod/mapper/VodMapper.java
@@ -0,0 +1,15 @@
+package com.puuaru.vod.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.puuaru.vod.properties.VodProperties;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Description: VodMapper
+ * @Author: puuaru
+ * @Date: 2023/1/6
+ */
+@Mapper
+public interface VodMapper extends BaseMapper<VodProperties> {
+
+}
