766e66737265689347bba1e655a37c12c3676246
Puuaru <honghuizhang4w@gmail.com>
2023-03-09 15:43:38 +0800

    feat(statistic): add data init scheduling

--- service/service-statistic/src/main/java/com/puuaru/statistic/StatisticServiceMain8007.java
+++ service/service-statistic/src/main/java/com/puuaru/statistic/StatisticServiceMain8007.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @Description: StatisticServiceMain8007
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.puuaru")
 @EnableFeignClients
+@EnableScheduling
 public class StatisticServiceMain8007 {
     public static void main(String[] args) {
         SpringApplication.run(StatisticServiceMain8007.class, args);
