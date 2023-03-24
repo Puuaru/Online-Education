ff00b5e1c386ef744fc43b693466d8c3553a94f9
Puuaru <honghuizhang4w@gmail.com>
2023-03-24 13:18:56 +0800

    feat(statistic): register count

--- service/service-center/src/main/java/com/puuaru/center/aspect/MessageAspect.java
+++ service/service-center/src/main/java/com/puuaru/center/aspect/MessageAspect.java
@@ -9,11 +9,9 @@ import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
-import reactor.util.function.Tuple2;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -24,18 +22,20 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 @Aspect
 @Component
-public class LoginAspect {
+public class MessageAspect {
+    private static final String REDIS_HASH_NAME = "count_stat";
+    private static final String LOGIN_COUNTER = "login_num";
+    private static final String REGISTER_COUNTER = "register_num";
+
+    private static final int LOGIN_CACHE_THRESHOLD = 128;
+    private static final int REGISTER_CACHE_THRESHOLD = 64;
+
     private final ThreadPoolExecutor threadPoolExecutor;
     private final StreamBridge streamBridge;
     private final RedisTemplate redisTemplate;
 
-    private final String REDIS_HASH_NAME = "count_stat";
-    private final String REDIS_HASH_KEY = "login_num";
-
-    private final int CACHE_THRESHOLD = 128;
-
     @Autowired
-    public LoginAspect(ThreadPoolExecutor threadPoolExecutor, StreamBridge streamBridge, RedisTemplate redisTemplate) {
+    public MessageAspect(ThreadPoolExecutor threadPoolExecutor, StreamBridge streamBridge, RedisTemplate redisTemplate) {
         this.threadPoolExecutor = threadPoolExecutor;
         this.streamBridge = streamBridge;
         this.redisTemplate = redisTemplate;
@@ -49,21 +49,35 @@ public class LoginAspect {
     public void githubLogin() {
     }
 
+    @Pointcut("execution(public * com.puuaru.center.controller.UcenterMemberController.register(..))")
+    public void register() {
+    }
+
     @SneakyThrows
     @Before("localLogin() || githubLogin()")
     public void loginCountHandler() {
-        Integer loginCount = (Integer) redisTemplate.opsForHash().get(REDIS_HASH_NAME, REDIS_HASH_KEY);
+        updateCounterWithNotification(LOGIN_COUNTER, LOGIN_CACHE_THRESHOLD);
+    }
+
+    @SneakyThrows
+    @Before("register()")
+    public void registerCountHandler() {
+        updateCounterWithNotification(REGISTER_COUNTER, REGISTER_CACHE_THRESHOLD);
+    }
+
+    private void updateCounterWithNotification(String counterType, Integer threshold) {
+        Integer count = (Integer) redisTemplate.opsForHash().get(REDIS_HASH_NAME, counterType);
         // 读取并更新计数，通过位运算求余
-        loginCount = loginCount == null ? 1 : (loginCount + 1) & (CACHE_THRESHOLD - 1);
+        count = count == null ? 1 : (count + 1) & (threshold - 1);
         // 更新计数器
-        redisTemplate.opsForHash().put(REDIS_HASH_NAME, REDIS_HASH_KEY, loginCount);
+        redisTemplate.opsForHash().put(REDIS_HASH_NAME, counterType, count);
         HashMap<String, String> params = new HashMap<>();
-        params.put("type", "login_num");
+        params.put("type", counterType);
         params.put("date", LocalDate.now(ZoneId.of("GMT-8")).toString());
-        if (loginCount == 0) {
+        if (count == 0) {
             // 到达阈值，则发送消息通知 Statistics 模块更新数据
             threadPoolExecutor.execute(() -> {
-                streamBridge.send("loginCount-out-0", MessageBuilder.withPayload(params).build());
+                streamBridge.send("updateStatistics-out-0", MessageBuilder.withPayload(params).build());
             });
         }
     }
