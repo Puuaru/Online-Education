fbf18791107ba72d1d4f9d0f70f71e432da32a80
Puuaru <honghuizhang4w@gmail.com>
2023-04-07 22:31:31 +0800

    feat(gateway): add CORS configuration in Spring Cloud Gateway

--- infrastracture/gateway/src/main/java/com/puuaru/gateway/GatewayMain8080.java
+++ infrastracture/gateway/src/main/java/com/puuaru/gateway/GatewayMain8080.java
@@ -2,6 +2,7 @@ package com.puuaru.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @Date: 2023/4/7
  */
 @SpringBootApplication
+@EnableFeignClients
 @ComponentScan(basePackages = "com.puuaru")
 public class GatewayMain8080 {
     public static void main(String[] args) {
---
+++ infrastracture/gateway/src/main/java/com/puuaru/gateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.puuaru.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @Description: CorsConfig
+ * @Author: puuaru
+ * @Date: 2023/4/7
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        CorsWebFilter corsWebFilter = new CorsWebFilter(source);
+        return corsWebFilter;
+    }
+}
