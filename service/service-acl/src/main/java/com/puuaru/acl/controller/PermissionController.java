ffea6adefac5c220f6d4852c9ab42422494eb924
Puuaru <honghuizhang4w@gmail.com>
2023-05-11 01:00:43 +0800

    feat(acl): add permission insert & update

--- service/service-acl/src/main/java/com/puuaru/acl/controller/PermissionController.java
+++ service/service-acl/src/main/java/com/puuaru/acl/controller/PermissionController.java
@@ -3,6 +3,7 @@ package com.puuaru.acl.controller;
 
 import com.puuaru.acl.entity.Permission;
 import com.puuaru.acl.service.PermissionService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,8 @@ import java.util.List;
 
 /**
  * <p>
- * 权限 前端控制器
+ * 权限 前端控制器,
+ * 控制权限与菜单
  * </p>
  *
  * @author puuaru
@@ -19,6 +21,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/acl/permission")
+@Api("菜单及权限控制")
 public class PermissionController {
 
     private final PermissionService permissionService;
@@ -48,5 +51,26 @@ public class PermissionController {
         permissionService.deletePermissionById(permissionId);
     }
 
+    /**
+     * 插入菜单或权限
+     * @param permission
+     * @return
+     */
+    @PostMapping("/add")
+    @ApiOperation("插入菜单或权限")
+    public Boolean savePermission(Permission permission) {
+        return permissionService.save(permission);
+    }
+
+    /**
+     * 更新菜单或权限
+     * @param permission
+     * @return
+     */
+    @PutMapping("/update")
+    @ApiOperation("更新菜单或权限")
+    public Boolean updatePermission(Permission permission) {
+        return permissionService.updateById(permission);
+    }
 }
 
